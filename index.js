cd78e902baedbd16728518ba5be2fbcc48ca564f
kaelzhang <i@kael.me>
2014-08-07 16:45:09 +0800

    migrate old projects

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ skema.Skema = Skema;
 var node_path = require('path');
 var node_url = require('url');
 
-function skema (attrs, options){
-    return new Skema(attrs, options);
+function skema(attrs, options) {
+  return new Skema(attrs, options);
 };
 
 
@@ -15,46 +15,46 @@ function skema (attrs, options){
 // @param {Object} options
 // - host: {Object} host object
 function Skema(attrs, options) {
-    options = options || {};
-    this.host = options.context || this;
-    this.__attrs = {};
+  options = options || {};
+  this.host = options.context || this;
+  this.__attrs = {};
 
-    this.key_list = [];
+  this.key_list = [];
 
-    var key;
+  var key;
 
-    for(key in attrs){
-        this.add(key, attrs[key]);
-    }
+  for (key in attrs) {
+    this.add(key, attrs[key]);
+  }
 }
 
 var TYPES = {
-    path: {
-        validator: function (v) {
-            return v || v === '';
-        },
-
-        setter: function (v) {
-            return node_path.resolve( String(v) );
-        }
+  path: {
+    validator: function(v) {
+      return v || v === '';
     },
 
-    url: {
-        validator: function (v) {
-            return !!node_url.parse(v).host;
-        }
+    setter: function(v) {
+      return node_path.resolve(String(v));
     }
+  },
+
+  url: {
+    validator: function(v) {
+      return !!node_url.parse(v).host;
+    }
+  }
 };
 
 
-[Number, String, Boolean].forEach(function (constructor) {
-    var name = constructor.name.toLowerCase();
+[Number, String, Boolean].forEach(function(constructor) {
+  var name = constructor.name.toLowerCase();
 
-    TYPES[name] = {
-        setter: function (v) {
-            return constructor(v);
-        }
+  TYPES[name] = {
+    setter: function(v) {
+      return constructor(v);
     }
+  }
 });
 
 
@@ -69,196 +69,196 @@ Skema.TYPES = TYPES;
 
 
 Skema.prototype = {
-    _get: function (key) {
-        var attr = this.__attrs[key];
+  _get: function(key) {
+    var attr = this.__attrs[key];
 
-        if(!attr){
-            return;
-        }
-
-        var getter = attr.type.getter;
-        var value = attr.value;
-    
-        return getter ?
-              // getter could based on the value of the current value
-            getter.call(this.host, value, key, this) :
-            value;
-    },
-
-    get: function (key) {
-        if(arguments.length === 0){
-            var ret = {};
-            var self = this;
-
-            // if get all values, only get enumerable properties
-            this.enumerable().forEach(function (key) {
-                ret[key] = self._get(key);
-            });
-
-            return ret;
-        }else{
-            return this._get(key);
-        }
-    },
-
-    // Reset to default value.
-    // Run this method will execute setup methods
-    reset: function (key) {
-        if(arguments.length === 0){
-            // reset all values, including non-enumerable and readonly properties
-            this.key_list.forEach(function (key) {
-                this._reset(key);
-            }, this);
-        
-        }else{
-            this._reset(key);
-        }
-    },
-
-    // Force to unsetting a value by key,
-    // use this method carefully.
-    _reset: function (key) {
-        var attr = this.__attrs[key];
-
-        if(!attr){
-            return;
-        }
-
-        var value = attr.value = attr._origin;
-        var setup = attr.type.setup; 
-
-        if(setup){
-            setup.call(this.host, value, key, this);
-        }
-    },
-
-    enumerable: function () {
-        var attrs = this.__attrs;
-
-        return this.key_list.filter(function (key) {
-            var attr = attrs[key];
-
-            return attr.enumerable ||
-                // Or not specified (default to true)
-                !('enumerable' in attr)
-        });
-    },
-
-    writable: function () {
-        var attrs = this.__attrs;
-
-        return this.key_list.filter(function (key) {
-            var attr = attrs[key];
-
-            return attr.writable ||
-                !('writable' in attr)
-        });
-    },
-
-    // @param {boolean=} ghost, ghost set, skip validation
-    _set: function (key, value, ghost) {
-        var attr = this.__attrs[key];
-        var is_success = false;
-
-        // for inner use
-        if(ghost){
-            attr.value = value;
-            return;
-        }
-
-        if(!attr){
-            return is_success;
-        }
-
-        // `attr.writable` is `true` by default
-        if(!attr.writable && 'writable' in attr){
-            return is_success;
-        }
-
-        // reset error status
-        delete this._err;
-
-        var validator = attr.type.validator;
-
-        if(!validator || validator.call(this.host, value, key, this)){
-
-            // validator returns true, but there might be error messages
-            if( is_success = !this._err ){
-                var setter = attr.type.setter;
-
-                if(setter){
-                    value = setter.call(this.host, value, key, this);
-
-                    is_success = !this._err;
-                }
-            }
-        // validator returns false
-        }else{
-            is_success = false;
-        }
-
-        if(is_success){
-            attr.value = value;
-        }
-
-        return is_success;
-    },
-
-    set: function (key, value) {
-        if(Object(key) === key){
-            var list = key;
-            var is_success = true;
-            var errors = {};
-            var result;
-
-            for(key in list){
-                value = list[key];
-                is_success = this._set(key, value) && is_success;
-            }
-
-            return is_success;
-
-        }else if(typeof key === 'string'){
-            return this._set(key, value);
-
-        }else{
-            return false;
-        }
-    },
-
-    // @param {Object} attr
-    // - value: {mixed} default value
-    // - type:
-    add: function (key, attr) {
-        if(key in this.__attrs){
-            return;
-        }
-
-        // save the original value
-        attr._origin = attr.value;
-
-        attr = attr || {};
-
-        var type = attr.type || {};
-
-        if(typeof type === 'string'){
-            type = Skema.TYPES[type];
-        }
-
-        if(Object(type) !== type){
-            return;
-        }
-
-        this.key_list.push(key);
-        attr.type = type;
-
-        this.__attrs[key] = attr;
-        this.reset(key);
-
-        return true;
-    },
-
-    error: function () {
-        this._err = true;
+    if (!attr) {
+      return;
     }
+
+    var getter = attr.type.getter;
+    var value = attr.value;
+
+    return getter ?
+      // getter could based on the value of the current value
+      getter.call(this.host, value, key, this) :
+      value;
+  },
+
+  get: function(key) {
+    if (arguments.length === 0) {
+      var ret = {};
+      var self = this;
+
+      // if get all values, only get enumerable properties
+      this.enumerable().forEach(function(key) {
+        ret[key] = self._get(key);
+      });
+
+      return ret;
+    } else {
+      return this._get(key);
+    }
+  },
+
+  // Reset to default value.
+  // Run this method will execute setup methods
+  reset: function(key) {
+    if (arguments.length === 0) {
+      // reset all values, including non-enumerable and readonly properties
+      this.key_list.forEach(function(key) {
+        this._reset(key);
+      }, this);
+
+    } else {
+      this._reset(key);
+    }
+  },
+
+  // Force to unsetting a value by key,
+  // use this method carefully.
+  _reset: function(key) {
+    var attr = this.__attrs[key];
+
+    if (!attr) {
+      return;
+    }
+
+    var value = attr.value = attr._origin;
+    var setup = attr.type.setup;
+
+    if (setup) {
+      setup.call(this.host, value, key, this);
+    }
+  },
+
+  enumerable: function() {
+    var attrs = this.__attrs;
+
+    return this.key_list.filter(function(key) {
+      var attr = attrs[key];
+
+      return attr.enumerable ||
+        // Or not specified (default to true)
+        !('enumerable' in attr)
+    });
+  },
+
+  writable: function() {
+    var attrs = this.__attrs;
+
+    return this.key_list.filter(function(key) {
+      var attr = attrs[key];
+
+      return attr.writable ||
+        !('writable' in attr)
+    });
+  },
+
+  // @param {boolean=} ghost, ghost set, skip validation
+  _set: function(key, value, ghost) {
+    var attr = this.__attrs[key];
+    var is_success = false;
+
+    // for inner use
+    if (ghost) {
+      attr.value = value;
+      return;
+    }
+
+    if (!attr) {
+      return is_success;
+    }
+
+    // `attr.writable` is `true` by default
+    if (!attr.writable && 'writable' in attr) {
+      return is_success;
+    }
+
+    // reset error status
+    delete this._err;
+
+    var validator = attr.type.validator;
+
+    if (!validator || validator.call(this.host, value, key, this)) {
+
+      // validator returns true, but there might be error messages
+      if (is_success = !this._err) {
+        var setter = attr.type.setter;
+
+        if (setter) {
+          value = setter.call(this.host, value, key, this);
+
+          is_success = !this._err;
+        }
+      }
+      // validator returns false
+    } else {
+      is_success = false;
+    }
+
+    if (is_success) {
+      attr.value = value;
+    }
+
+    return is_success;
+  },
+
+  set: function(key, value) {
+    if (Object(key) === key) {
+      var list = key;
+      var is_success = true;
+      var errors = {};
+      var result;
+
+      for (key in list) {
+        value = list[key];
+        is_success = this._set(key, value) && is_success;
+      }
+
+      return is_success;
+
+    } else if (typeof key === 'string') {
+      return this._set(key, value);
+
+    } else {
+      return false;
+    }
+  },
+
+  // @param {Object} attr
+  // - value: {mixed} default value
+  // - type:
+  add: function(key, attr) {
+    if (key in this.__attrs) {
+      return;
+    }
+
+    // save the original value
+    attr._origin = attr.value;
+
+    attr = attr || {};
+
+    var type = attr.type || {};
+
+    if (typeof type === 'string') {
+      type = Skema.TYPES[type];
+    }
+
+    if (Object(type) !== type) {
+      return;
+    }
+
+    this.key_list.push(key);
+    attr.type = type;
+
+    this.__attrs[key] = attr;
+    this.reset(key);
+
+    return true;
+  },
+
+  error: function() {
+    this._err = true;
+  }
 };
--- test/skema.js
+++ test/skema.js
@@ -4,178 +4,177 @@ var expect = require('chai').expect;
 var skema = require('../index');
 
 var attr = skema({
-    a: {
-        type: 'string'
-    },
+  a: {
+    type: 'string'
+  },
 
-    b: {
-        type: 'number'
-    },
+  b: {
+    type: 'number'
+  },
 
-    c: {
-        type: 'boolean'
-    },
+  c: {
+    type: 'boolean'
+  },
 
-    d: {
-        type: 'path'
-    },
+  d: {
+    type: 'path'
+  },
 
-    e: {
-        type: 'url'
-    },
+  e: {
+    type: 'url'
+  },
 
-    f: {
+  f: {
 
-    },
+  },
 
-    g: {
-        value: 'abc'
-    },
+  g: {
+    value: 'abc'
+  },
 
-    h: {
-        value: 'abc',
-        enumerable: false
-    },
+  h: {
+    value: 'abc',
+    enumerable: false
+  },
 
-    h2: {
-        value: 'abc',
-        enumerable: true
-    },
+  h2: {
+    value: 'abc',
+    enumerable: true
+  },
 
-    h3: {
-        value: 'abc'
-    },
+  h3: {
+    value: 'abc'
+  },
 
-    i: {
-        value: 'abc',
-        writable: false
-    },
+  i: {
+    value: 'abc',
+    writable: false
+  },
 
-    i2: {
-        value: 'abc',
-        writable: true
-    },
+  i2: {
+    value: 'abc',
+    writable: true
+  },
 
-    i3: {
-        value: 'abc'
-    }
+  i3: {
+    value: 'abc'
+  }
 });
 
-describe("basic types", function(){
-    it("string type", function(){
-        attr.set('a', '1');
-        expect(attr.get('a')).to.equal('1');
-    });
+describe("basic types", function() {
+  it("string type", function() {
+    attr.set('a', '1');
+    expect(attr.get('a')).to.equal('1');
+  });
 
-    it("string type, convertion:", function(){
-        attr.set('a', 1);
-        expect(attr.get('a')).to.equal('1');
-        expect(attr.get('a')).not.to.equal(1);
-    });
+  it("string type, convertion:", function() {
+    attr.set('a', 1);
+    expect(attr.get('a')).to.equal('1');
+    expect(attr.get('a')).not.to.equal(1);
+  });
 
-    it("number", function(){
-        attr.set('b', 1);
-        expect(attr.get('b')).to.equal(1);
+  it("number", function() {
+    attr.set('b', 1);
+    expect(attr.get('b')).to.equal(1);
 
-        attr.set('b', '1');
-        expect(attr.get('b')).to.equal(1);
-    });
+    attr.set('b', '1');
+    expect(attr.get('b')).to.equal(1);
+  });
 
-    it("boolean", function(){
-        attr.set('c', 1);
-        expect(attr.get('c')).to.equal(true);
+  it("boolean", function() {
+    attr.set('c', 1);
+    expect(attr.get('c')).to.equal(true);
 
-        attr.set('c', 0);
-        expect(attr.get('c')).to.equal(false);
-    });
+    attr.set('c', 0);
+    expect(attr.get('c')).to.equal(false);
+  });
 
-    it("path", function(){
-        var node_path = require('path');
-        var value = 'abc';
-        var real_value = node_path.resolve(value);
+  it("path", function() {
+    var node_path = require('path');
+    var value = 'abc';
+    var real_value = node_path.resolve(value);
 
-        attr.set('d', value);
-        expect(attr.get('d')).to.equal(real_value);
-    });
+    attr.set('d', value);
+    expect(attr.get('d')).to.equal(real_value);
+  });
 
-    it("url", function(){
-        expect(attr.set('e', 'abc.com')).to.equal(false)
-    });
+  it("url", function() {
+    expect(attr.set('e', 'abc.com')).to.equal(false)
+  });
 
-    it("no type", function(){
-        var a = {};
-        attr.set('f', a);
-        expect(attr.get('f')).to.equal(a);
-    });
-});
-
-
-describe("default value", function(){
-    it("default", function(){
-        expect(attr.get('g')).to.equal('abc');
-    });
-
-    it(".reset()", function(){
-        attr.set('g', 123);
-        expect(attr.get('g')).to.equal(123);
-
-        attr.reset('g');
-        expect(attr.get('g')).to.equal('abc');
-    });
+  it("no type", function() {
+    var a = {};
+    attr.set('f', a);
+    expect(attr.get('f')).to.equal(a);
+  });
 });
 
 
-describe("enumerable:", function(){
-    var list = attr.enumerable();
-    var all = attr.get();
+describe("default value", function() {
+  it("default", function() {
+    expect(attr.get('g')).to.equal('abc');
+  });
 
-    it("false", function(){
-        expect( list.indexOf('h') ).to.equal(-1);
+  it(".reset()", function() {
+    attr.set('g', 123);
+    expect(attr.get('g')).to.equal(123);
 
-        attr.set('h', 1);
-        expect(attr.get('h')).to.equal(1);
-        expect('h' in all).to.equal(false);
-    });
-
-    it("true", function(){
-        expect( list.indexOf('h2') ).not.to.equal(-1);
-
-        attr.set('h2', 1);
-        expect(attr.get('h2')).to.equal(1);
-        expect('h2' in all).to.equal(true);
-    });
-
-    it("(not set)", function(){
-        expect( list.indexOf('h3') ).not.to.equal(-1);
-
-        attr.set('h3', 1);
-        expect(attr.get('h3')).to.equal(1);
-        expect('h3' in all).to.equal(true);
-    });
+    attr.reset('g');
+    expect(attr.get('g')).to.equal('abc');
+  });
 });
 
 
-describe("writable:", function(){
-    var list = attr.writable();
+describe("enumerable:", function() {
+  var list = attr.enumerable();
+  var all = attr.get();
 
-    it("false", function(){
-        expect( list.indexOf('i') ).to.equal(-1);
+  it("false", function() {
+    expect(list.indexOf('h')).to.equal(-1);
 
-        expect(attr.set('i', 1)).to.equal(false);
-        expect(attr.get('i')).to.equal('abc');
-    });
+    attr.set('h', 1);
+    expect(attr.get('h')).to.equal(1);
+    expect('h' in all).to.equal(false);
+  });
 
-    it("true", function(){
-        expect( list.indexOf('i2') ).not.to.equal(-1);
+  it("true", function() {
+    expect(list.indexOf('h2')).not.to.equal(-1);
 
-        expect(attr.set('i2', 1)).to.equal(true);
-        expect(attr.get('i2')).to.equal(1);
-    });
+    attr.set('h2', 1);
+    expect(attr.get('h2')).to.equal(1);
+    expect('h2' in all).to.equal(true);
+  });
 
-    it("(not set)", function(){
-        expect( list.indexOf('i3') ).not.to.equal(-1);
-        expect(attr.set('i3', 1)).to.equal(true);
-        expect(attr.get('i3')).to.equal(1);
-    });
+  it("(not set)", function() {
+    expect(list.indexOf('h3')).not.to.equal(-1);
+
+    attr.set('h3', 1);
+    expect(attr.get('h3')).to.equal(1);
+    expect('h3' in all).to.equal(true);
+  });
 });
 
+
+describe("writable:", function() {
+  var list = attr.writable();
+
+  it("false", function() {
+    expect(list.indexOf('i')).to.equal(-1);
+
+    expect(attr.set('i', 1)).to.equal(false);
+    expect(attr.get('i')).to.equal('abc');
+  });
+
+  it("true", function() {
+    expect(list.indexOf('i2')).not.to.equal(-1);
+
+    expect(attr.set('i2', 1)).to.equal(true);
+    expect(attr.get('i2')).to.equal(1);
+  });
+
+  it("(not set)", function() {
+    expect(list.indexOf('i3')).not.to.equal(-1);
+    expect(attr.set('i3', 1)).to.equal(true);
+    expect(attr.get('i3')).to.equal(1);
+  });
+});
